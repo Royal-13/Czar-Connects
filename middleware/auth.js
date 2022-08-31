8cdf9c87a9b8b7b0b69d1718589e10d3f1ab882b
Royal-13 <87161414+Royal-13@users.noreply.github.com>
2022-08-31 16:02:06 +0530

    Files Upload

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, "AC1C3E5820F38350B1A76F51C89149CFF10B9E672B9D5096907B1EB9ECCDC8C1", (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
